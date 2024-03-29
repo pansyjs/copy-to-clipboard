e20cb433e1dc39d822ff913d558721497c7aa45e
wangxingkang <wang_xingkang@qq.com>
2024-03-29 14:06:30 +0800

    chore: test

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -1,6 +1,7 @@
 import { execSync } from 'node:child_process'
 import path from 'node:path'
 import { consola } from 'consola'
+import { PATHS } from './.internal/constants';
 import { version } from '../package.json'
 
 execSync('npm run build', { stdio: 'inherit' })
@@ -16,6 +17,6 @@ if (version.includes('beta'))
   command += ' --tag beta'
 
 for (const name of packages) {
-  execSync(command, { stdio: 'inherit', cwd: path.join('packages', name, 'dist') })
+  execSync(command, { stdio: 'inherit', cwd: path.join(PATHS.PACKAGES, name) })
   consola.success(`Published @pansy/${name}`)
 }
